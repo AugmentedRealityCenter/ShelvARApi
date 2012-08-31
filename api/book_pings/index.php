05cc73fa2a934aa85c6e15a6eff62f778490c842
Bo Brinkman <Bo.Brinkman@muohio.edu>
2012-08-31 15:28:05 -0400

    Starting re-org of book_pings collection

---
+++ api/book_pings/index.php
@@ -0,0 +1,3 @@
+This script will return the data for a single book ping, based on id. URL will be:
+http://dev.shelvar.com/api/book_pings/12345.json
+Use .htaccess to block non-json formats, and to make the 12345 the id to be looked up
