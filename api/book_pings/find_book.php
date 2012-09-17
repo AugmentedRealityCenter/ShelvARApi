fd2e0cb045e7c160c9df36b4a1247df7fc8d2a94
Raavian Rehman <rehmanra@muohio.edu>
2012-09-17 15:14:12 -0400

    Prepared Statement Conversion find_book.php
    Needs tested.

--- api/book_pings/find_book.php
+++ api/book_pings/find_book.php
@@ -5,49 +5,69 @@
  * Find where the book was most recently
  *
  * Copyright 2011 by ShelvAR Team.
- * 
- * @version Nov 6, 2011
- * @author Eliot Fowler
+ *
+ * @version September 17, 2012
+ * @author Eliot Fowler and Raavian Rehman
  */
- 
- //find_book("QA76.7 .A25 1991");
+
+//find_book("QA76.7 .A25 1991");
 /**
-* Takes a string as input and (if connecting to the database is successful) queries the database 
-* for all instances where that call number was seen. All of the tuples returned are stored in an 
-* array. The last row in the array (the most recent ping) is returned as an associative array.
-*
-* @return An associative array with three values: last_time_seen, left_neighbor, right_neighbor.
-*/
- function find_book($lcNum)
- {
-   include_once "../../db_info.php";
-   $con = mysql_connect($server,$user,$password);
-	if (!$con){
-		Print "COULDNT CONNECT";
-		die('Could not connect: ' . mysql_error());
+ * Takes a string as input and (if connecting to the database is successful) queries the database
+ * for all instances where that call number was seen. All of the tuples returned are stored in an
+ * array. The last row in the array (the most recent ping) is returned as an associative array.
+ *
+ * @return An associative array with three values: last_time_seen, left_neighbor, right_neighbor.
+ */
+function find_book($lcNum)
+{
+	include_once "../../db_info.php";
+	 
+	/* Create a new mysqli object with database connection parameters */
+	$con = new mysqli($server, $user, $password, $database);
+
+	if(mysqli_connect_errno()) {
+		echo "Connection Failed: " . mysqli_connect_errno();
+		exit();
 	}
-  
-	mysql_select_db($database, $con);
-	
-	$resource = mysql_query("SELECT * FROM book_pings " . 
-	          "WHERE book_call = '" . $lcNum . "'");
-			  
-	if($resource == FALSE){
+	/* Create a prepared statement */
+	if($stmt = $mysqli -> prepare("SELECT * FROM book_pings WHERE book_call =?")) {
+
+		/* Bind parameters
+		 s - string, b - blob, i - int, etc */
+		$stmt -> bind_param("s", $lcNum);
+
+		/* Execute it */
+		$stmt -> execute();
+
+		/* Bind results */
+		$stmt -> bind_result($result);
+
+		/* Fetch the value */
+		$stmt -> fetch();
+
+		/* Close statement */
+		$stmt -> close();
+	}
+	 
+		
+	if($result == FALSE){
 		Print "FAILED 1";
-      //Print 'SQL Select failed' . mysql_error();
-    } else if(mysql_num_rows($resource) == 0) {
+		//Print 'SQL Select failed' . mysqli_error();
+	} else if(mysqli_num_rows($result) == 0) {
 		Print "NO ROWS";
-      //Print 'No rows seleted';
-    } else { 
+		//Print 'No rows seleted';
+	} else {
 		//Print $resource;
-		while ($row = mysql_fetch_assoc($resource)) {
+		while ($row = mysqli_fetch_assoc($result)) {
 			$arr[] = $row;
 		}
-		
+
 		$return_arr["last_time_seen"] = $arr[count($arr)-1]["ping_time"];
 		$return_arr["left_neighbor"] = $arr[count($arr)-1]["neighbor1_call"];
 		$return_arr["right_neighbor"] = $arr[count($arr)-1]["neighbor2_call"];
+		/* Close connection */
+		$mysqli -> close();
 		return $return_arr;
 	}
- }
+}
 ?>
