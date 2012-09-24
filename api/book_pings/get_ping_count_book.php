dac8e2dc535972f964cf80590fc73f0b25beee0f
Eliot Fowler <fowlerje@muohio.edu>
2012-09-24 15:49:51 -0400

    error checking

--- api/book_pings/get_ping_count_book.php
+++ api/book_pings/get_ping_count_book.php
@@ -1,7 +1,7 @@
 <?php
 include_once "../../db_info.php";
-//if(isset($_GET("book_tag")
-	//$book_tag = $_GET("book_tag");
+if(isset($_GET("book_tag")))
+	$book_tag = $_GET("book_tag");
 /*$call_number = $_GET('call_number');
 $neighbor_tag = $_GET('neighbor_tag');
 $neighbor_call = $_GET('neighbor_call');
