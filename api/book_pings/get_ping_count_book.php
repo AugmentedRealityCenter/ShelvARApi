e1970466a79ebe460d86e792a4166c2189cec2e8
Eliot Fowler <fowlerje@muohio.edu>
2012-09-30 23:31:45 -0400

    checking dynamic binding

--- api/book_pings/get_ping_count_book.php
+++ api/book_pings/get_ping_count_book.php
@@ -65,14 +65,14 @@ var_dump($bindParam->get());
 		if($stmt = $con -> prepare($sql)) {
 			// Bind parameters
 			 //s - string, b - blob, i - int, etc
-			 echo "hihi";
+			 
 			 call_user_func_array(array($this->stmt, "bind_param"),$bindParam->get()); 
 			//call_user_func_array( array($stmt, 'bind_param'), $bindParam->get());
 			//$stmt -> bind_param('ss', $book_tag, $call_number);
 
 			//Execute it
 			$stmt -> execute();
-
+echo "hihi";
 			// Bind results
 			$stmt -> bind_result($result);
 
