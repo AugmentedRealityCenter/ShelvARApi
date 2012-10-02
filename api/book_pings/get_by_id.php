e1fff7783d5d26c45f71eede9494e6d275f3ce9d
Bo Brinkman <Bo.Brinkman@muohio.edu>
2012-10-01 21:02:28 -0400

    Updated to use new database.php from Raavian

--- api/book_pings/get_by_id.php
+++ api/book_pings/get_by_id.php
@@ -1,134 +1,31 @@
 <?php
 
-/**
- * @file get_by_id.php
- * @author Jake Rego
- * Copyright 2012 by ShelvAR Team.
- * @version September 29, 2012
- * Retrieves a book_ping with the database id that is entered
- * The id entered, according to the db format, should be an 11 character integer
- */
- include "book_ping_lib.php"
- include ("../../db_info.php");
- 
- // getting the entered ID and limiting by institution
- $ret = get_by_id(stripslashes($_GET["book_ping_id"],$_GET["institution"]); 
- 
- /** alternate query format
- $ret_two = get_by_id_1(stripslashes($_GET["book_ping_id"],$_GET["institution"]);
- */
- 
- /** 
- * Queries the database for the requested book_ping_id 
- */
- function get_by_id($book_ping_id, $institution="") {
- 
-	 if(isset($_GET["book_ping_id"]))
-		$book_ping_id = $_GET['book_ping_id'];
-	 
-	 // Create a new mysqli object with database connection parameters
-		$server = "localhost";
-		$user ="mysql_username";
-		$password = "mysql_pword";
-		$database = "mysql_db";
-		$con = new mysqli($server, $user, $password, $database);
-		
-		if(mysqli_connect_errno()) {
-			echo "Connection Failed: " . mysqli_connect_errno();
-			exit();
-		}
-		
-		/* create a prepared statement */
-		if($stmt - $con -> prepare("SELECT * from book_pings WHERE id = " + $book_ping_id) { //&& query_or_not ==true) {
-		
-			// Bind parameters
-			$stmt -> bind_param("ssssssss",
-			$book_info["book_tag"],
-			$book_info["book_call"],
-			$book_info["neighbor1_tag"],
-			$book_info["neighbor1_call"],
-			$book_info["neighbor2_tag"],
-			$book_info["neighbor2_call"],
-			$book_info["ping_time"],
-			$institution);
-		
-			// Execute it
-			$stmt -> execute();
-		
-			// Bind results
-			$stmt -> bind_result($query_result);
-			
-			// Fetch value
-			$stmt -> fetch();
-			
-			// Close statement
-			$stmt -> close();		
-		}
+  /**
+   * @file get_by_id.php
+   * @author Jake Rego and Bo Brinkman
+   * Copyright 2012 by ShelvAR Team.
+   * @version September 29, 2012
+   * Retrieves a book_ping with the database id that is entered
+   * The id entered, according to the db format, should be an 11 character integer
+   */
 
-		if($query_result == FALSE) {
-			Print 'SQL Select failed' . mysqli_error();
-		} else {
-			while ($row = mysqli_fetch_assoc($res2[$res_type = MYSQL_ASSOC ])) {
-			
-				
-				/* If returned as array, should fill book_info array */
-				$book_info[0] = $book_ping_id;
-				$book_info[1] = $row["book_tag"];
-				$book_info[2] = $row["book_call"];
-				$book_info[3] = $row["neighbor1_tag"];
-				$book_info[4] = $row["neighbor1_call"];
-				$book_info[5] = $row["neighbor2_tag"];
-				$book_info[6] = $row["neighbor2_call"];
-				$book_info[7] = $row["ping_time"];
-				$book_info[8] = $row["institution"];			
-				
-			}
-			mysqli_free_result($res2);
-		}
-			
-		/* Close connection */
-		$con -> close();
-		
-	$book_info = json_encode(array('item' => $book_info_string), JSON_FORCE_OBJECT);	
-	
-	return $book_info;
-} 
- 
- 
- /** 
- * Queries the database in a different format, not sure how to check institution here
- */
- function get_by_id_1($book_ping_id, $institution="") {
-	
-	if(isset($_GET["book_ping_id"]))
-		$book_ping_id = $_GET['book_ping_id'];
-	 
-	 // Create a new mysqli object with database connection parameters
-		$server = "localhost";
-		$user ="mysql_username";
-		$password = "mysql_pword";
-		$database = "mysql_db";
-		$con = new mysqli($server, $user, $password, $database);
-		
-		if(mysqli_connect_errno()) {
-			echo "Connection Failed: " . mysqli_connect_errno();
-			exit();
-		}
-		
-		// creating query statement and executing
-		$query = "SELECT * FROM book_pings WHERE id =" + $book_ping_id;
-		if( $con -> mysqli_fetch_array($query) ) {
-			$sql_output = mysqli_fetch_array($query); //should be a string of the row		
-		}
-		
-		// closes connection
-		$con -> close();
-		
-		$sql_output = json_encode($sql_output);
-		return $sql_output;		
+include "../../database.php";
+
+$array = array();
+$db = new database();
+print($db->query . "\n\n");
+$db->query = "SELECT * FROM book_pings WHERE id = ? and institution = ?";
+$db->params = array($_GET['book_ping_id'],$_GET['institution']);
+$db->type = 'is';
+$the_rec = $db->fetch();
+
+if(count($the_rec)>0){
+  unset($the_rec[0]['institution']);
+  $arr = array('book_ping' => $the_rec[0], 'result'=>"SUCCESS");
+  print json_encode($arr);
+} else {
+  $arr = array('book_ping' => "", 'result'=>"ERROR No such book ping id.");
+  print json_encode($arr);
  }
- 
- 
 
-return $ret; // or $ret_two
  ?>
