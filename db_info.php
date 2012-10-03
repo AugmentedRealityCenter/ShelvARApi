9bbf7f69607019afe8af3e30582ee52485c6401b
Bo Brinkman <Bo.Brinkman@muohio.edu>
2012-10-03 16:32:53 -0400

    Revert eliot's changes to default db_info.php

--- db_info.php
+++ db_info.php
@@ -4,7 +4,7 @@
  * with a remote database, so leaving $server set to localhost is recommended.
  */
 $server = "localhost";
-$user ="root";
-$password = "Store@433";
-$database = "shelvar";
+$user ="mysql_username";
+$password = "mysql_pword";
+$database = "mysql_db";
 ?>
