d3b7978ca02712bc4e5a26654dc06e90e752344a
Andrew Bair <bairac@muohio.edu>
2012-10-26 14:53:59 -0400

    call_number changes 2
    fixes call_number to return proper format in var call_number

--- api/lc2bin/call_numbers.php
+++ api/lc2bin/call_numbers.php
@@ -20,17 +20,20 @@ $binret = LC2Bin($JSONparsedArr["lcNum"]);
 
 $result = ($JSONparsedArr["allow"]=="true") ? "SUCCESS" : "ERROR";
 
+$c_num_arr = array( ($JSONparsedArr["lcNum"]["alphabetic"].$JSONparsedArr["lcNum"]["wholeClass"]) );
+if(strlen($JSONparsedArr["lcNum"]["decClass"])>0) array_push($c_num_arr, (".".$JSONparsedArr["lcNum"]["decClass"]));
+if(strlen($JSONparsedArr["lcNum"]["date1"])>0) array_push($c_num_arr, (" ".$JSONparsedArr["lcNum"]["date1"]) );
+if(strlen($JSONparsedArr["lcNum"]["cutter1"])>0) array_push($c_num_arr, (" .".$JSONparsedArr["lcNum"]["cutter1"]) );
+if(strlen($JSONparsedArr["lcNum"]["date2"])>0) array_push($c_num_arr, (" ".$JSONparsedArr["lcNum"]["date2"]) );
+if(strlen($JSONparsedArr["lcNum"]["cutter2"])>0) array_push($c_num_arr, (" ".$JSONparsedArr["lcNum"]["cutter2"]) );
+if(strlen($JSONparsedArr["lcNum"]["element8"])>0) array_push($c_num_arr, (" ".$JSONparsedArr["lcNum"]["element8"]) );
+if(strlen($JSONparsedArr["lcNum"]["element9"])>0) array_push($c_num_arr, (" ".$JSONparsedArr["lcNum"]["element9"]) );
+if(strlen($JSONparsedArr["lcNum"]["element10"])>0) array_push($c_num_arr, (" ".$JSONparsedArr["lcNum"]["element10"]) );
+
+$call_number = implode($c_num_arr);
+
 echo json_encode(array("book_tag"=>bin2base64($binret['Bin']), 
-						"call_number"=>($JSONparsedArr["lcNum"]["alphabetic"] . 
-										$JSONparsedArr["lcNum"]["wholeClass"] . "." .
-										$JSONparsedArr["lcNum"]["decClass"] . 
-										$JSONparsedArr["lcNum"]["date1"] .
-										$JSONparsedArr["lcNum"]["cutter1"] .
-										$JSONparsedArr["lcNum"]["date2"] .
-										$JSONparsedArr["lcNum"]["cutter2"] .
-										$JSONparsedArr["lcNum"]["element8"] .
-										$JSONparsedArr["lcNum"]["element9"] .
-										$JSONparsedArr["lcNum"]["element10"]), 
+						"call_number"=>$call_number, 
 						"parsed_call_number"=>$JSONparsedArr["lcNum"],
 						"parser_feedback"=>$JSONparsedArr["arrOfConflicts"], "result"=>$result));
 
