76603ec255961501142ce4802f23a5c4362ef605
Andrew Bair <bairac@muohio.edu>
2012-10-26 11:31:31 -0400

    call_numbers fix
    fix to "call_number" returned var

--- api/lc2bin/call_numbers.php
+++ api/lc2bin/call_numbers.php
@@ -11,7 +11,6 @@
 include_once "LC_Converter_lib.php";
 include_once "../base64_lib.php";
 include_once "../lcparse/parseLibrary.php";
-include_once "../../header_include.php";
 
 $JSONparsed = urldecode($_GET["call_number"]);
 
@@ -21,7 +20,18 @@ $binret = LC2Bin($JSONparsedArr["lcNum"]);
 
 $result = ($JSONparsedArr["allow"]=="true") ? "SUCCESS" : "ERROR";
 
-echo json_encode(array("book_tag"=>bin2base64($binret['Bin']), "call_number"=>$JSONparsed, "parsed_call_number"=>$JSONparsedArr["lcNum"],
+echo json_encode(array("book_tag"=>bin2base64($binret['Bin']), 
+						"call_number"=>($JSONparsedArr["lcNum"]["alphabetic"] . 
+										$JSONparsedArr["lcNum"]["wholeClass"] . "." .
+										$JSONparsedArr["lcNum"]["decClass"] . 
+										$JSONparsedArr["lcNum"]["date1"] .
+										$JSONparsedArr["lcNum"]["cutter1"] .
+										$JSONparsedArr["lcNum"]["date2"] .
+										$JSONparsedArr["lcNum"]["cutter2"] .
+										$JSONparsedArr["lcNum"]["element8"] .
+										$JSONparsedArr["lcNum"]["element9"] .
+										$JSONparsedArr["lcNum"]["element10"]), 
+						"parsed_call_number"=>$JSONparsedArr["lcNum"],
 						"parser_feedback"=>$JSONparsedArr["arrOfConflicts"], "result"=>$result));
 
  ?>
