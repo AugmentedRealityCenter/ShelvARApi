d7f6e22524f649688cf4ecca893b27b540528fea
Justin Ordway <ordwayjm@miamioh.edu>
2013-03-19 23:42:04 -0400

    Removed connect.php

--- connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$dbhost = "localhost";   
-	$dbname = "shelvar"; 	
-	$dbuser = "";  
-	$dbpass = "";
-	
-	mysql_connect($dbhost, $dbuser, $dbpass) or die("MySQL Error: " . mysql_error());  
-	mysql_select_db($dbname) or die("MySQL Error: " . mysql_error());
-?>
