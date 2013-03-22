f85d60eea15ed10b6aded79d352da54c1f0fa109
Drew T. Ritcher <ritchedt@muohio.edu>
2013-03-22 13:15:05 -0400

    inst and users sql tables in one sql file

--- setup/create_users_inst_tables.sql
+++ setup/create_users_inst_tables.sql
@@ -19,3 +19,22 @@ CREATE TABLE `users` (
   CONSTRAINT `inst_id` FOREIGN KEY (`inst_id`) REFERENCES `institution` (`inst_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8$$
 
+
+
+CREATE TABLE `institution` (
+  `inst_id` varchar(20) NOT NULL,
+  `name` varchar(140) NOT NULL,
+  `admin_contact` varchar(45) NOT NULL,
+  `inst_type` int(6) NOT NULL,
+  `inst_size` int(6) NOT NULL,
+  `is_activated` binary(1) NOT NULL,
+  `exp_date` datetime NOT NULL,
+  `num_api_calls` int(11) NOT NULL,
+  `inst_num` int(11) NOT NULL AUTO_INCREMENT,
+  `alt_contact` varchar(500) NOT NULL,
+  `inst_url` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`inst_num`),
+  UNIQUE KEY `inst_id_UNIQUE` (`inst_id`),
+  KEY `inst_id_idx` (`inst_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8$$
+
