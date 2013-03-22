4437394f3036bd8f2c1dc2b07461baeb7a82fa98
Bo Brinkman <Bo.Brinkman@MiamiOH.edu>
2013-03-21 20:50:14 -0400

    Got rid of some junk and comments, and added IF NOT EXISTS

--- setup/shelvar.sql
+++ setup/shelvar.sql
@@ -1,27 +1,4 @@
--- MySQL dump 10.13  Distrib 5.1.62, for debian-linux-gnu (i486)
---
--- Host: localhost    Database: shelvar
--- ------------------------------------------------------
--- Server version	5.1.62-0ubuntu0.10.04.1
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `book_pings`
---
-
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `book_pings` (
+CREATE TABLE IF NOT EXISTS `book_pings` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `book_tag` varchar(40) NOT NULL,
   `book_call` varchar(240) NOT NULL,
@@ -32,9 +9,9 @@ CREATE TABLE `book_pings` (
   `ping_time` datetime NOT NULL,
   `institution` varchar(40) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=108082 DEFAULT CHARSET=latin1;
+);
 
-CREATE TABLE `institutions` (
+CREATE TABLE IF NOT EXISTS `institutions` (
   `inst_id` varchar(20) NOT NULL,
   `inst_num` int(11) NOT NULL AUTO_INCREMENT,
   `description` varchar(140) NOT NULL,
@@ -49,7 +26,7 @@ CREATE TABLE `institutions` (
   KEY (`inst_num`)
 );
 
-CREATE TABLE `users` (
+CREATE TABLE IF NOT EXISTS `users` (
   `user_id` varchar(45) NOT NULL UNIQUE,
   `inst_id` varchar(20) NOT NULL,
   `user_num` int(11) NOT NULL AUTO_INCREMENT,
@@ -65,15 +42,3 @@ CREATE TABLE `users` (
   KEY (`user_num`),
   FOREIGN KEY (inst_id) REFERENCES institutions(inst_id)
 );
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2012-06-06 10:21:42
