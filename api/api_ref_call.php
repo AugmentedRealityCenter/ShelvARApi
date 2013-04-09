9a6c6c4e73429d8c1807030afb81e6c13ffbdf13
Bo Brinkman <Bo.Brinkman@MiamiOH.edu>
2013-04-09 00:16:02 +0000

    Add missing dependency

--- api/api_ref_call.php
+++ api/api_ref_call.php
@@ -1,6 +1,8 @@
 <?php
 
 require_once($_SERVER['DOCUMENT_ROOT'] . '/oauth/AutoLoader.php');
+require_once($_SERVER['DOCUMENT_ROOT'] . '/oauth/OAuthProviderWrapper.php');
+
 new AutoLoader();
 
 $Provider 	= new OAuthProviderWrapper(OAuthProviderWrapper::TOKEN_VERIFY	);
