fbed03acc20c0639c44391fbe70cfe8be87e9f4d
Bo Brinkman <Bo.Brinkman@MiamiOH.edu>
2013-04-12 21:05:40 -0400

    Create institutions before users

--- setup/create_users_inst_tables.sql
+++ setup/create_users_inst_tables.sql
@@ -1,3 +1,20 @@
+CREATE TABLE `institutions` (
+  `inst_id` varchar(20) NOT NULL,
+  `name` varchar(140) NOT NULL,
+  `admin_contact` varchar(45) NOT NULL,
+  `inst_type` int(6) NOT NULL,
+  `inst_size` int(6) NOT NULL,
+  `is_activated` binary(1) NOT NULL,
+  `exp_date` datetime NOT NULL,
+  `num_api_calls` int(11) NOT NULL,
+  `inst_num` int(11) NOT NULL AUTO_INCREMENT,
+  `alt_contact` varchar(500) NOT NULL,
+  `inst_url` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`inst_num`),
+  UNIQUE KEY `inst_id_UNIQUE` (`inst_id`),
+  KEY `inst_id_idx` (`inst_id`)
+);
+
 CREATE TABLE `users` (
   `inst_id` varchar(20) NOT NULL,
   `password` char(64) NOT NULL,
@@ -15,21 +32,4 @@ CREATE TABLE `users` (
   KEY `inst_id_idx` (`inst_id`),
   KEY `inst_id_idx1` (`inst_id`),
   CONSTRAINT `inst_id` FOREIGN KEY (`inst_id`) REFERENCES `institutions` (`inst_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-);
-
-CREATE TABLE `institutions` (
-  `inst_id` varchar(20) NOT NULL,
-  `name` varchar(140) NOT NULL,
-  `admin_contact` varchar(45) NOT NULL,
-  `inst_type` int(6) NOT NULL,
-  `inst_size` int(6) NOT NULL,
-  `is_activated` binary(1) NOT NULL,
-  `exp_date` datetime NOT NULL,
-  `num_api_calls` int(11) NOT NULL,
-  `inst_num` int(11) NOT NULL AUTO_INCREMENT,
-  `alt_contact` varchar(500) NOT NULL,
-  `inst_url` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`inst_num`),
-  UNIQUE KEY `inst_id_UNIQUE` (`inst_id`),
-  KEY `inst_id_idx` (`inst_id`)
 );
