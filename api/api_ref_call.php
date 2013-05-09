badc8cb3b5419bd5b10259d7a28293e55e436e8d
Bo Brinkman <Bo.Brinkman@MiamiOH.edu>
2013-05-09 16:30:34 -0400

    whoami for currently authenticated user

--- api/api_ref_call.php
+++ api/api_ref_call.php
@@ -8,7 +8,6 @@ new AutoLoader();
 $Provider 	= new OAuthProviderWrapper(OAuthProviderWrapper::TOKEN_VERIFY	);
 $response 	= $Provider->checkOAuthRequest();
 if (!is_bool($response) || $response != true) {
-	//echo $response;
   exit(json_encode(array("result"=>"ERROR. OAuth token missing or invalid.")));
 }
 
--- api/oauth/whoami.php
+++ api/oauth/whoami.php
@@ -1,8 +1,26 @@
 <?php
-include_once('../api_ref_call.php');
+include_once "../api_ref_call.php";
+include_once "../../database.php";
+include_once "../../header_include.php";
 
-echo("<pre>");
-var_dump($Provider);
-echo("</pre>");
+try {
+  $user_num = $Provider->getUserId();
+  $db = new database();
+  $db->query = "SELECT * FROM users WHERE user_num = ?";
+  $db->params = array($user_num);
+  $db->type = "i";
+  $the_rec = $db->fetch();
+
+  if(count($the_rec) > 0){
+    $arr = array('user_id' => $the_rec[0]['user_id'], 'inst_id'=>$the_rec[0]['inst_id'],'result'=>'SUCCESS');
+    print(json_encode($arr));
+  } else {
+    $arr = array('user_id'=>"", 'inst_id'=>"", 'result' => "ERROR No user id found.");
+    print(json_encode($arr));
+  }
+
+} catch (Exception $Exception) {
+  exit(json_encode(array("result"=>"ERROR. OAuth token missing or invalid.")));
+}
 
 ?>
