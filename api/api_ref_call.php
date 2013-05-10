432122fa77b23b4c36759551db7cd6867f651115
Bo Brinkman <Bo.Brinkman@MiamiOH.edu>
2013-05-10 10:15:14 -0400

    Add sandbox user

--- api/api_ref_call.php
+++ api/api_ref_call.php
@@ -5,31 +5,60 @@ require_once($_SERVER['DOCUMENT_ROOT'] . '/oauth/OAuthProviderWrapper.php');
 
 new AutoLoader();
 
+unset($user);
+
 $Provider 	= new OAuthProviderWrapper(OAuthProviderWrapper::TOKEN_VERIFY	);
 $response 	= $Provider->checkOAuthRequest();
-if (!is_bool($response) || $response != true) {
+
+if(is_bool($response) && $response == true){
+  //Do nothing
+ } else if(stripos($_SERVER['HTTP_REFERER'],"api.shelvar.com") !== false){
+  $user_id = "sandy";
+  try {
+    $db = new database();
+    $db->query = "SELECT * FROM users WHERE user_id = ?";
+    $db->params = array($user_id);
+    $db->type = "s";
+    $the_rec = $db->fetch();
+
+    if(count($the_rec) > 0){
+      $user = $the_rec[0];
+      $user_id = $user['user_id'];
+      $inst_id = $user['inst_id'];
+    } else if{
+      $arr = array('result' => "ERROR User not found.");
+      exit(json_encode($arr));
+    }
+
+  } catch (Exception $Exception) {
+    exit(json_encode(array("result"=>"ERROR Could not find Sandbox user.")));
+  }
+  
+ } else if (!is_bool($response) || $response != true) {
   exit(json_encode(array("result"=>"ERROR. OAuth token missing or invalid.")));
 }
 
-try {
-  $user_num = $Provider->getUserId();
-  $db = new database();
-  $db->query = "SELECT * FROM users WHERE user_num = ?";
-  $db->params = array($user_num);
-  $db->type = "i";
-  $the_rec = $db->fetch();
+if(!isset($user)){
+  try {
+    $user_num = $Provider->getUserId();
+    $db = new database();
+    $db->query = "SELECT * FROM users WHERE user_num = ?";
+    $db->params = array($user_num);
+    $db->type = "i";
+    $the_rec = $db->fetch();
 
-  if(count($the_rec) > 0){
-    $user = $the_rec[0];
-    $user_id = $user['user_id'];
-    $inst_id = $user['inst_id'];
-  } else {
-    $arr = array('result' => "ERROR User not found.");
-    exit(json_encode($arr));
+    if(count($the_rec) > 0){
+      $user = $the_rec[0];
+      $user_id = $user['user_id'];
+      $inst_id = $user['inst_id'];
+    } else if{
+      $arr = array('result' => "ERROR User not found.");
+      exit(json_encode($arr));
+    }
+
+  } catch (Exception $Exception) {
+    exit(json_encode(array("result"=>"ERROR OAuth token missing or invalid.")));
   }
-
-} catch (Exception $Exception) {
-  exit(json_encode(array("result"=>"ERROR OAuth token missing or invalid.")));
-}
+ }
 
 ?>
