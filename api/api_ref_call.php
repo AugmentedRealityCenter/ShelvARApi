74a8fea6bf9c27db3cd900f9da54109ccb34d6ef
Bo Brinkman <Bo.Brinkman@MiamiOH.edu>
2013-05-10 09:31:36 -0400

    Move oauth access points

--- api/api_ref_call.php
+++ api/api_ref_call.php
@@ -11,4 +11,23 @@ if (!is_bool($response) || $response != true) {
   exit(json_encode(array("result"=>"ERROR. OAuth token missing or invalid.")));
 }
 
+try {
+  $user_num = $Provider->getUserId();
+  $db = new database();
+  $db->query = "SELECT * FROM users WHERE user_num = ?";
+  $db->params = array($user_num);
+  $db->type = "i";
+  $the_rec = $db->fetch();
+
+  if(count($the_rec) > 0){
+    $user = $the_rect[0];
+  } else {
+    $arr = array('result' => "ERROR User not found.");
+    exit(json_encode($arr));
+  }
+
+} catch (Exception $Exception) {
+  exit(json_encode(array("result"=>"ERROR OAuth token missing or invalid.")));
+}
+
 ?>
--- api/oauth/whoami.php
+++ api/oauth/whoami.php
@@ -3,24 +3,7 @@ include_once "../api_ref_call.php";
 include_once "../../database.php";
 include_once "../../header_include.php";
 
-try {
-  $user_num = $Provider->getUserId();
-  $db = new database();
-  $db->query = "SELECT * FROM users WHERE user_num = ?";
-  $db->params = array($user_num);
-  $db->type = "i";
-  $the_rec = $db->fetch();
-
-  if(count($the_rec) > 0){
-    $arr = array('user_id' => $the_rec[0]['user_id'], 'inst_id'=>$the_rec[0]['inst_id'],'result'=>'SUCCESS');
+$arr = array('user_id' => $user['user_id'], 'inst_id'=>$user['inst_id'], 'is_admin' => $user['is_admin'], 'can_submit_data' => $user['can_submit_data'], 'can_read_data' => $user['can_read_data'], 'is_superadmin' => '0', 'result'=>'SUCCESS');
     print(json_encode($arr));
-  } else {
-    $arr = array('user_id'=>"", 'inst_id'=>"", 'result' => "ERROR No user id found.");
-    print(json_encode($arr));
-  }
-
-} catch (Exception $Exception) {
-  exit(json_encode(array("result"=>"ERROR. OAuth token missing or invalid.")));
-}
 
 ?>
