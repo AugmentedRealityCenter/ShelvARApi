e01554656de8a58de39cf82f928c57e9552f2a56
Bo Brinkman <Bo.Brinkman@MiamiOH.edu>
2013-05-13 16:39:55 -0400

    Update to use new variable name

--- api/oauth/whoami.php
+++ api/oauth/whoami.php
@@ -3,7 +3,7 @@ include_once "../api_ref_call.php";
 include_once "../../database.php";
 include_once "../../header_include.php";
 
-$arr = array('user_id' => $user['user_id'], 'inst_id'=>$user['inst_id'], 'is_admin' => $user['is_admin'], 'can_submit_data' => $user['can_submit_data'], 'can_read_data' => $user['can_read_data'], 'is_superadmin' => '0', 'result'=>'SUCCESS');
+$arr = array('user_id' => $oauth_user['user_id'], 'inst_id'=>$oauth_user['inst_id'], 'is_admin' => $oauth_user['is_admin'], 'can_submit_data' => $oauth_user['can_submit_data'], 'can_read_data' => $oauth_user['can_read_data'], 'is_superadmin' => '0', 'result'=>'SUCCESS');
     print(json_encode($arr));
 
 ?>
