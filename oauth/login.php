aa8877c97b63b9237e25ba0b1ecc79f760c20430
Bo Brinkman <Bo.Brinkman@MiamiOH.edu>
2013-05-14 15:25:06 -0400

    Set proper experation on access tokens

--- oauth/login.php
+++ oauth/login.php
@@ -45,6 +45,16 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && !empty($_POST['login'])) {
       exit;
     }
 
+    $db = new database();
+    $db->query = "SELECT exp_date FROM institutions WHERE inst_id = ?";
+    $inst_id = $result[0]['inst_id'];
+    $db->params = array($inst_id);
+    $db->type = 's';
+    $res2 = $db->fetch();
+
+    session_start();
+    $_SESSION['user_num'] = $result[0]['user_num'];
+
     echo("<html><head><meta http-equiv=\"refresh\" content=\"0;post_login?oauth_token=" . $_GET['oauth_token'] . "\"></head></html>");
     exit(200);
   }
