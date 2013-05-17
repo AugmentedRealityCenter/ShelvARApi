fac2f482c67ed9f80f2b667057896bd913535f5b
Bo Brinkman <Bo.Brinkman@MiamiOH.edu>
2013-05-17 16:15:11 -0400

    Tag printer revamp

---
+++ api/tagmaker/pdfPrint2.php
@@ -0,0 +1,64 @@
+<?php
+require_once('helper/fpdf.php');
+require_once('../lc2bin/lc_numbers_lib.php');
+include_once "../HammingCode.php";
+
+/** GLOBAL VARS **/
+//array of callNumbers to print
+$tagsParam = json_decode($_GET['tags']);
+//requested sheet type
+$sheetTypeParam = $_GET['type'];
+		
+//grab the different label options and put them in $sheetValues
+$paper_format = fetchOptions()[urldecode($sheetTypeParam)];
+
+$pdf = new FPDF($paper_format['orientation'],$paper_format['units'],array($paper_format['width'],$paper_format['height']));
+
+make_logo($pdf);
+
+$pdf->Output( ($sheetType['name'] . ".pdf"), "I");
+		
+/**
+ * Grab the available label sheet options
+ **/
+function fetchOptions(){
+  $tempValues = file('tagformats.json', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+  return json_decode($tempValues);
+}
+		
+/***
+ * Make the ShelvAR logo
+ *     31x13
+ ***/
+function make_logo($pdf){
+  
+  $logoArr = array(array(1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1),
+		   array(1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1),
+		   array(1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1),
+		   array(1,0,1,0,0,0,1,0,1,1,1,1,1,1,1,0,1,1,1,1,1,0,0,1,1,0,0,0,1,0,1),
+		   array(1,0,1,0,1,1,1,0,0,0,1,0,0,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1),
+		   array(1,0,1,0,0,1,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,0,0,1,0,0,1,1,0,1),
+		   array(1,0,1,1,0,0,1,0,1,0,1,0,0,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1),
+		   array(1,0,1,1,1,0,1,0,1,0,1,0,1,1,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1),
+		   array(1,0,1,0,0,0,1,0,1,0,1,0,0,0,1,0,1,0,0,1,1,0,1,0,1,0,1,0,1,0,1),
+		   array(1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1),
+		   array(1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1),
+		   array(1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1),
+		   array(1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1)
+		   );
+  
+  $scale = .5;
+  for($i=0; $i < 13; $i++){
+    for($j=0; $j < 31; $j++){
+      if(1 == $logoArr[$i][$j]){
+	$pdf->Rect(($j * $scale) + 10,
+		   ($i * $scale) + 285,
+		   $scale,
+		   $scale,
+		   'F'); //  X, Y, W, H, Fill 
+      }
+    }
+  }
+}
+
+?>
