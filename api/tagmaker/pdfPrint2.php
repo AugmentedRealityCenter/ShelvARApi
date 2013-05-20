6651b24ae90b5d9a7e9819f64b219ad51e3e40e6
Bo Brinkman <Bo.Brinkman@MiamiOH.edu>
2013-05-20 15:50:14 -0400

    Merging of short lines

--- api/tagmaker/pdfPrint2.php
+++ api/tagmaker/pdfPrint2.php
@@ -88,7 +88,6 @@ function make_num($left, $bottom, $top, $pdf, $paper_format, $tag){
     }
     $joined = $lc_parts[$parts_index];
     if(count($processed_parts) > 0){
-      error_log(print_r($processed_parts,true));
       $joined = $processed_parts[count($processed_parts)-1] . " " . $joined;
     }
     if(count($processed_parts) > 0 && $pdf->GetStringWidth($joined) <= $paper_format->tag_width){
