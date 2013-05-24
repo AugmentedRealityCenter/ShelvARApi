cd841df76b289f6be0f72f7e8e59f9f574d693ba
Bo Brinkman <Bo.Brinkman@MiamiOH.edu>
2013-05-24 16:21:36 -0400

    More permissions checking

--- api/book_pings/get_ping_count_book.php
+++ api/book_pings/get_ping_count_book.php
@@ -3,6 +3,15 @@ include_once ("../../db_info.php");
 include_once "../../header_include.php";
 
 include_once "../api_ref_call.php";
+if($oauth_user['inst_activated'] != 1){
+  exit(json_encode(array('result'=>'ERROR Your institution\'s account has not yet been activated.')));
+ }
+if($oauth_user['inst_has_inv'] != 1){
+  exit(json_encode(array('result'=>'ERROR Your institution does not subscribe to ShelvAR\'s inventory service.')));
+ }
+if($oauth_user['email_verified'] != 1){
+  exit(json_encode(array('result'=>'ERROR You have not yet verified your email address.')));
+ }
 if($oauth_user['can_read_inv'] != 1){
   exit(json_encode(array('result'=>'ERROR No permission to read data.')));
  }
