01d6cbded0234a7193e166c8a4e78eda37d6b6ce
ordwayjm <ordwayjm@miamioh.edu>
2013-06-03 12:39:55 -0400

    added script to figure out how to get domain name

--- send_activation_email.php
+++ send_activation_email.php
@@ -5,13 +5,13 @@
 	else $to = $pending_email;
 	
 	$api = "https://api.shelvar.com/";
-	if($_SERVER['HTTP_HOST'] == "dev.shelvar.com") {
+	if($_SERVER['SERVER_NAME'] == "dev.shelvar.com") {
 		$api = "http://devapi.shelvar.com/";
 	}
 	
 	$subject = "ShelvAR.com Registration";
 
-	$message = "Welcome to ShelvAR!\r\rYou, or someone using your email address, has completed registration at ShelvAR.com. You can complete registration by clicking the following link:\r$api?$activation_key";
+	$message = "Welcome to ShelvAR!\r\rYou, or someone using your email address, has completed registration at ShelvAR.com. You can complete registration by clicking the following link:\r$api/activate_email.php?$activation_key";
 
 	$headers = 'From: noreply@shelvar.com' . "\r\n" .
     		   'Reply-To: noreply@shelvar.com' . "\r\n" .
---
+++ server_info.php
@@ -0,0 +1,4 @@
+<?php
+	echo $_SERVER['SERVER_NAME'];
+	echo $_SERVER['HTTP_HOST'];
+?>
