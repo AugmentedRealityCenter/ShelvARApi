eba96266bf5325d6eb57ca91e6bc17a4ddd1144b
ordwayjm <ordwayjm@miamioh.edu>
2013-06-05 11:27:50 -0400

    added get_user script

---
+++ api/users/get_user.php
@@ -0,0 +1,31 @@
+<?php
+	include_once $_SERVER['DOCUMENT_ROOT'] . "/database.php";
+	include_once $_SERVER['DOCUMENT_ROOT'] . "/header_include.php";
+	include_once $_SERVER['DOCUMENT_ROOT'] . "/api/api_ref_call.php";
+	
+	$err = array();
+	
+	if(!$_GET['user_id']) {
+		$err[] = "No user_id supplied";
+	}
+	if($_GET['user_id'] != $oauth_user['user_id']) {
+		$err[] = "Invalid access to user account";
+	}
+	if(!count($err)) {
+		$user_id = $_GET['user_id'];
+		
+		$db = new database();
+		$db->query = "SELECT user_id, name, email FROM users WHERE user_id = ?";
+		$db->params = array($user_id);
+		$db->type = 's';
+		
+		$result = $db->fetch();
+		if(!empty($result)) {
+			echo json_encode(array('result'=>"SUCCESS", 'user'=>$result, 'errors'=>""));
+		}
+		else $err[] = "SQL Error";
+	}
+	if($err) {
+		echo json_encode(array('result'=>"ERROR", 'user'=>"", 'errors'=>$err)); 
+	}
+?>
