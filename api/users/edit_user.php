13eba606c87a5675c812ac45851cc76ff933ebb0
ordwayjm <ordwayjm@miamioh.edu>
2013-06-11 10:33:20 -0400

    check if either get or post are set

--- api/users/edit_user.php
+++ api/users/edit_user.php
@@ -5,25 +5,48 @@
 	
 	$err = array();
 	
+	$user_id = "";
 	if(!$_POST['user_id']) {
-		$err[] = "No user_id supplied";
+		if(!$_GET['user_id']) {
+			$err[] = "No user_id supplied";
+		}
+		else $user_id = $_GET['user_id'];
 	}
-	if($_POST['user_id'] != $oauth_user['user_id']) {
+	else $user_id = $_POST['user_id'];
+	
+	if($user_id != $oauth_user['user_id']) {
 		$err[] = "Invalid access to user account";
 	}
 	if(!count($err)) {
 		$query = "UPDATE users SET ";
 		$params = array();
-		$user_id = $_POST['user_id'];
+		
 		if(isset($_POST['name'])) {
 			$query += "name = ?,";
 			$params[] = $_POST['name'];
 		}
+		else if(isset($_GET['name'])) {
+			$query += "name = ?,";
+			$params[] = $_GET['name'];
+		}
+		
 		if(isset($_POST['email'])) {
 			$query += "email = ?,";
 			$params[] = $_POST['email'];
 		}
-		if(isset($_POST['password'])) {
+		else if(isset($_GET['email'])) {
+			$query += "email = ?,";
+			$params[] = $_GET['email'];
+		}
+		
+		if(isset($_POST['password']) {
+			$password = $_POST['password'];
+		}
+		else if(isset($_GET['password']) {
+			$password = $_GET['password'];
+		}
+		
+		if(isset($_POST['password']) || isset($_GET['password'])) {
 			$db = new database();
 			$db->query = "SELECT encrip_salt FROM users WHERE user_id = ?";
 			$db->params = array($user_id);
@@ -34,10 +57,11 @@
 			// TODO error handling
 			
 			$query += "password = ?,";
-			$password = $_POST['password'];
+			
 			$password = hash('sha256', $password . $salt);
 			$params[] = $password;
-		}
+		}	
+		
 		$query = substr($query,0,-1); // removing trailing comma
 		$query += "WHERE user_id = ?";
 		$params[] = $user_id;
