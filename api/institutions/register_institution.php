89ec8b4f0080563d1a5aa182ad131b347907a797
Justin Ordway <ordwayjm@miamioh.edu>
2013-06-13 12:26:06 -0400

    register institution script

--- api/institutions/register_institution.php
+++ api/institutions/register_institution.php
@@ -18,14 +18,23 @@
 	// If there are no errors
 	if(!count($err)) {
 		$inst_id = $_POST['inst_id'];
-		$description = $_POST['description'];
-		$admin_contact = $_POST['admin_contact'];
-		$alt_contact = $_POST['alt_contact'];
+		$name = $_POST['inst_name'];
+		$admin_contact = "";
 		$inst_type = $_POST['inst_type'];
 		$inst_size = $_POST['inst_size'];
+		$is_activated = 0;
+		$has_inv = 0;
+		// set initial exp_date to 1 month after registering
+		$today = date("Y-m-d H:i:s");
+		$exp_date = strtotime(date("Y-m-d H:i:s", strtotime($today)) . "+1 month");
+		$num_api_calls = 0;
+		$alt_contact = $_POST['alt_contact'];
+		$inst_url = $_POST['inst_url'];
+		$pending_email = $_POST['email'];
+		$email_verified = 0;
 		
 		$db = new database();
-		$db->query = "SELECT * FROM institutions WHERE inst_id = ?";
+		$db->query = "SELECT inst_id FROM institutions WHERE inst_id = ?";
 		$db->params = array($inst_id);
 		$db->type = 's';
 		
@@ -34,14 +43,30 @@
 			$err[]="Institution ID already taken";
 		}
 		else { 
+			// generate email activation key
+			do {
+				$activation_key = md5(uniqid(rand(), true));
+				$activation_key = substr($activation_key, 0, 64);
+			
+				$db = new database();
+				$db->query = "SELECT inst_id FROM institutions WHERE activation_key = ?";
+				$db->params = array($activation_key);
+				$db->type = 's';
+			
+				$result = $db->fetch();
+			} while(!empty($result));
+			
 			$db = new database();
-			$db->query = "INSERT INTO institutions(inst_id,description,admin_contact,alt_contact,inst_type,inst_size,is_activated,exp_date,num_api_calls)
-							VALUES(?,?,?,?,?,?,?,?,?)";
-			$db->params = array($inst_id, $description, $admin_contact, $alt_contact, $inst_type, $inst_size, 0, date('Y-m-d H:i:s'), 0);
-			$db->type = 'ssssssisi';
+			$db->query = "INSERT INTO institutions(inst_id,name,admin_contact,inst_type,inst_size,is_activated,has_inv,exp_date,num_api_calls,alt_contact,inst_url,pending_email,activation_key,email_verified)
+							VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+			$db->params = array($inst_id, $name, $admin_contact, $inst_type, $inst_size, $is_activated, $has_inv, $exp_date, $num_api_calls, $alt_contact, $inst_url,$pending_email,$activation_key,$email_verified);
+			$db->type = 'sssiiiisissssi';
 			
 			if($db->insert()) {
-				echo json_encode(array('result'=>"SUCCESS", 'inst_id'=>$inst_id));
+				include_once($_SERVER['DOCUMENT_ROOT'] . "/api/institutions/send_activation_email.php");
+				if(!$err) {
+					echo json_encode(array('result'=>"SUCCESS", 'inst_id'=>$inst_id));
+				}
 			}
 			else {
 				$err[] = "MySQL Error";
