f79f38be850ab13c4b7339bd79937401f2833062
Justin Ordway <ordwayjm@miamioh.edu>
2013-06-27 12:40:34 -0400

    email_registered script

---
+++ api/users/email_registered.php
@@ -0,0 +1,19 @@
+<?php
+	include_once "../../database.php";
+	include_once "../../header_include.php";
+	                                  
+	if(isset($_GET['email'])) {                                                               
+		$email = $_GET['email'];                                  
+		$db = new database();
+		$db->query = "SELECT user_id FROM users WHERE email = ?";
+		$db->params = array($email);
+		$db->type = 's';
+		
+		$result = $db->fetch();
+		if(!empty($result)) {
+			echo json_encode(array('result'=>"SUCCESS", 'user_id'=>"REGISTERED", 'message'=>"Email address registered with user", 'errors'=>""));
+		}
+		else echo json_encode(array('result'=>"SUCCESS", 'user_id'=>"NOT REGISTERED", 'message'=>"", 'errors'=>""));
+	}
+	else echo json_encode(array('result'=>"ERROR", 'errors'=>"No email address supplied"));
+?>
