f6756baf43c45ce312a14f2e25146caa245ca4fa
Justin Ordway <ordwayjm@miamioh.edu>
2013-06-28 10:32:12 -0400

    check if user email is already verified

--- api/users/activate_email.php
+++ api/users/activate_email.php
@@ -21,6 +21,9 @@
 		if(empty($result)) {
 			$err[] = "No user associated with supplied activation key";
 		}
+		if($result[0]['email_verified'] == 1) {
+			$err[] = "User already verified";
+		}
 		else {
 			$user_id = $result[0]['user_id'];
 			$pending_email = $result[0]['pending_email'];
@@ -29,7 +32,6 @@
 			$email = $pending_email;
 			$email_verified = 1;
 			$pending_email = "";
-			$activation_key = "";
 			$is_admin = 0;
 			
 			// check if email matches admin email in institutions to give admin rights
@@ -47,9 +49,9 @@
 			else $is_admin = 0;
 			
 			$db = new database();
-			$db->query = "UPDATE users SET pending_email = ?, email = ?, email_verified = ?, is_admin = ?, activation_key = ? WHERE user_id = ?";
-			$db->params = array($pending_email, $email, $email_verified, $is_admin, $activation_key, $user_id);
-			$db->type = 'ssiiss';
+			$db->query = "UPDATE users SET pending_email = ?, email = ?, email_verified = ?, is_admin = ? WHERE user_id = ?";
+			$db->params = array($pending_email, $email, $email_verified, $is_admin, $user_id);
+			$db->type = 'ssiis';
 			
 			if($db->update()) {
 				echo json_encode(array('result'=>"SUCCESS", 'user_id'=>$user_id, 'errors'=>""));
