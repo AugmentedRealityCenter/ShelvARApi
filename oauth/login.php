44238183c3772d7c6ad9183c417dcd8ec56447b2
Justin Ordway <ordwayjm@miamioh.edu>
2013-07-11 12:48:25 -0400

    fixed css

--- oauth/login.php
+++ oauth/login.php
@@ -105,7 +105,7 @@ echo(
 	</head>
 	<body>
 	  <div class="container">
-		<img src="../ShelvARLogo_Big.png" style="align-left"/>
+		<img src="../ShelvARLogo_Big.png" style="text-align: left"/>
 		<br/>
 		<div class="content">
 		  <div class="row">
