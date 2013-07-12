a14dae70e0b829732eaeb8b1b632625d2469015c
Justin Ordway <ordwayjm@miamioh.edu>
2013-07-12 12:19:12 -0400

    extra wording

--- oauth/post-login.php
+++ oauth/post-login.php
@@ -133,6 +133,7 @@ echo(
 			  <h3>An application is requesting access to your ShelvAR account</h3>
 			  <p>This application <b>will be able to</b>: </p>
 			  '.$scope.'
+			  <p>Allow access to your account?</p>
 			  <form method="POST" action="?oauth_token='.$RequestToken->getToken().'">
 				<fieldset>
 				  <button name="allow" class="btn" value="Allow" type="submit">Allow</button>
