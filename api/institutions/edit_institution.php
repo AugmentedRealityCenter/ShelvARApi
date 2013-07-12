119d82a6004ab895aa254eb79540eaaeaf8d1e67
Justin Ordway <ordwayjm@miamioh.edu>
2013-07-12 13:30:29 -0400

    Revert "consistent error message format for institutions"
    This reverts commit 8937f9787b1a1fbd6a80401ce4ac80fbad106b16.

--- api/institutions/edit_institution.php
+++ api/institutions/edit_institution.php
@@ -6,7 +6,7 @@
 	$err = array();
 	
 	if(stripos($oauth_user['scope'],"acctmod") === false) {
-		exit(json_encode(array('result'=>"ERROR", 'errors'=> "No permission to modify account")));
+		exit(json_encode(array('result'=>'ERROR No permission to modify account.')));
 	}
 	
 	$inst_id = "";
