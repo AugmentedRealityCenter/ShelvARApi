8f803ff6fc9b981f40495c5912fbff10c4e8c81f
Bo Brinkman <Bo.Brinkman@MiamiOH.edu>
2013-09-18 10:44:47 -0400

    Sandbox users automatically get permissions turned on

--- api/users/activate_email.php
+++ api/users/activate_email.php
@@ -33,7 +33,12 @@
 			$email_verified = 1;
 			$pending_email = "";
 			$is_admin = 0;
-			
+			$permissions = 0;
+			//Sandbox users get approved for everything except admin
+			if($inst_id == "sandbox"){
+			  $permissions = 1;
+			}
+
 			// check if email matches admin email in institutions to give admin rights
 			$db = new database();
 			$db->query = "SELECT admin_contact FROM institutions WHERE inst_id = ?";
@@ -49,9 +54,9 @@
 			else $is_admin = 0;
 			
 			$db = new database();
-			$db->query = "UPDATE users SET pending_email = ?, email = ?, email_verified = ?, is_admin = ? WHERE user_id = ?";
-			$db->params = array($pending_email, $email, $email_verified, $is_admin, $user_id);
-			$db->type = 'ssiis';
+			$db->query = "UPDATE users SET pending_email = ?, email = ?, email_verified = ?, is_admin = ?, can_submit_inv = ?, can_read_inv = ?, can_shelf_read = ? WHERE user_id = ?";
+			$db->params = array($pending_email, $email, $email_verified, $is_admin, $permissions, $permissions, $permissions, $user_id);
+			$db->type = 'ssiiiiis';
 			
 			if($db->update()) {
 				echo json_encode(array('result'=>"SUCCESS", 'user_id'=>$user_id, 'errors'=>""));
