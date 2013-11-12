f94816117baaa57c38f4999d048a986c0ee2b350
Steven Angles <anglessw@miamioh.edu>
2013-11-12 00:42:01 -0500

    Small spelling fixes for API doc and create table statement for notifications

--- doc/test.php
+++ doc/test.php
@@ -1,5 +1,22 @@
 <?php 
 	include_once $_SERVER['DOCUMENT_ROOT'] . "/database.php";
 	include_once $_SERVER['DOCUMENT_ROOT'] . "/header_include.php";
-	phpinfo();
+	
+	echo "Creating notifications table...";
+	$db = new mysqli($sql_server, $sql_user, $sql_password, $sql_database);
+	$query = "CREATE TABLE IF NOT EXISTS `notifications` (".
+				"`notif_id` int(11) NOT NULL AUTO_INCREMENT,".
+				"`text` varchar(240) NOT NULL,".
+				"`read` binary(1) NOT NULL,".
+				"`create_time` datetime NOT NULL,".
+				"`user_id` varchar(40) DEFAULT NULL,".
+				"`inst_id` varchar(40) NOT NULL,".
+				"PRIMARY KEY (`notif_id`)".
+				");";
+	if ($db->query($query) === TRUE) {
+		echo "done!";
+	} else {
+		echo "\nError ".$db->errno.": ".$db->error;
+	}
+	$db->close();
 ?>
