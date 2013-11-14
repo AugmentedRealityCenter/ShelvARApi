beba65690976822d7cca095571333d56e5d8a7e1
Steven Angles <anglessw@miamioh.edu>
2013-11-14 14:16:45 -0500

    testing get institution notifications

--- api/notifications/get_inst_notification.php
+++ api/notifications/get_inst_notification.php
@@ -21,25 +21,58 @@
 	if($inst_id != $oauth_user['inst_id'] || $oauth_user['is_superadmin'] == 0) {
 		$err[] = "Invalid access to institution account";
 	}
-	
+
 	// if all preliminary checks passed
 	if (!count($err)) {
-		$db = new database();
+		$cond = false;
+		$limSet = false;
+		$query = "SELECT * FROM notifications ";
+		$qArray = array();
+		$paramsList = array();
+		$types = "";
 		
-		$query = "SELECT notif_id,text,read,create_time
-					FROM notifications
-					WHERE inst_id = ?
-					GROUP BY notif_id";
-		$params = array($inst_id);
-		$type = "isis";
-		
-		$db->query = $query;
-		$db->params = $params;
-		$db->type = $type;
-		
+		if(isset($_GET["start_date"])){
+			$qArray[] = "create_time >= ?";
+			$paramsList[] = urldecode($_GET["start_date"]);
+			$cond = true;
+			$types .= "s";
+		}
+		if(isset($_GET["end_date"])){
+			$qArray[] = "create_time < ?";
+			$paramsList[] = urldecode($_GET["end_date"]);
+			$cond = true;
+			$types .= "s";
+		}
+		if(isset($_GET["num_limit"]) && (is_int($_GET["num_limit"]) || ctype_digit($_GET["num_limit"]))){
+			$paramsList[] = $_GET["num_limit"];
+			$cond = true;
+			$limSet = true;
+			$types .= "s";
+		}
+
+		if (!$cond) {
+			$query = $query . " LIMIT 0,20";
+			
+			$db = new database();
+			$db->query = $query;
+			$db->params = $paramsList;
+			$db->type = "";
+		}
+		else {
+			$query = $query . " WHERE ";
+			
+			$query .= implode(" AND ", $qArray);
+			if ($limSet)
+				$query = $query . " LIMIT 0,?";
+			else 
+				$query = $query . " LIMIT 0,20";
+			
+			$db = new database();
+			$db->query = $query;
+			$db->params = $paramsList;
+			$db->type = $types;
+		}
 		$result = $db->fetch();
-		
-		echo json_encode($result);
 	}
 	if($err) {
 		echo json_encode(array('result'=>"ERROR", 'errors'=>$err)); 
