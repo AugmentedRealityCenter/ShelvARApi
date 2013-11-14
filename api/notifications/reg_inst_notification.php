e36eec599c8cddf393bf1a409cdac121ca904a9e
Steven Angles <anglessw@miamioh.edu>
2013-11-14 11:16:36 -0500

    fixed date issue

--- api/notifications/reg_inst_notification.php
+++ api/notifications/reg_inst_notification.php
@@ -17,7 +17,7 @@
 	if (!count($err)) {
 		$inst_id = $_GET['inst_id'];
 		$text = htmlspecialchars($_GET['text'], ENT_HTML401);
-		$today = date("Y-m-d H:i:s");
+		$today = strtotime("now");
 		// TODO: insert this notification for all users who are admins of specified institution
 		$db = new database();
 		$db->query = "INSERT INTO notifications(text,read,create_time,user_id,inst_id)
