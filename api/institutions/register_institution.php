3bb6796979d6ddf03a1a4f2b8a43abd6bce57a52
Nanda Kishore, Kesanam <kesanan@miamioh.edu>
2014-02-11 17:18:39 -0500

    testing admin institution email

--- api/institutions/register_institution.php
+++ api/institutions/register_institution.php
@@ -84,7 +84,7 @@
 					$subject = "New Shelvar Institution Registered";
 					$message = "<img src='".$api."ShelvARLogo_Big.png' /><br/><br/>Dear ShelvAR Admin,<br/><br/>This email is to notify that a new institution has been registered on ShelvAR.<br/>
 																										   <br/>Institution Name: ".$name.
-																										   "<br/>Institution ID: ".$inst_id.
+																										   "<br/>Institution ID: ".$inst_id."<br/>".
 																										   "<br/>If this message was sent as a mistake you can safely ignore it.";
 					$headers = 'From: ShelvAR.com <noreply@shelvar.com>' . "\r\n" .
 							   'Reply-To: noreply@shelvar.com' . "\r\n" .
