7c171f8fbc103b2c470d32aa0daa8fadac61207b
Nanda Kishore, Kesanam <kesanan@miamioh.edu>
2014-03-06 17:11:28 -0500

    fixing more errors

--- oauth/reset_password.php
+++ oauth/reset_password.php
@@ -36,7 +36,16 @@
 					$subject = "Your temporary password";
 					$message = "<img src='".$api."ShelvARLogo_Big.png' /><br/><br/>Dear <br/>".$user_id."<br/>Your password to log into ShelvAR has been temporarily changed to ". $p. 
 																										"Please log in using this password and your username. At that time you may change your password to something more familiar.". "<br/>";
-					mail ($_POST['email'], $subject, $message, ‘From: admin@shelvar.com’);
+					
+					$headers = 'From: ShelvAR.com <noreply@shelvar.com>' . "\r\n" .
+							   'Reply-To: noreply@shelvar.com' . "\r\n" .
+							   'Content-type: text/html' . "\r\n" .
+							    'X-Mailer: PHP/' . phpversion();
+					
+					if(!mail ($_POST['email'], $subject, $message, $headers){
+						$err[] = "Error sending confirmation email";
+					}
+					
 					echo '<h3>Your password has been changed. You will receive the new, temporary password at the email address with which you registered. Once you have logged in with this password, you may change it by clicking on the \“Accounts and then User\” link.</h3>';
 				
 					session_start();
