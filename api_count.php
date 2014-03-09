f91452cb42ab68edab620fe7406c704efb267db0
John Martin <johnmartin1992@yahoo.com>
2014-03-09 15:41:32 -0400

    t: p i

--- api_count.php
+++ api_count.php
@@ -411,6 +411,7 @@ function handleIPAddress() {
 	$results = $db->fetch();
 	
 	if ($results == NULL || count($results) == 0) {
+		error_log("Got here");
 		$query = "INSERT INTO unknown_users(ip_address) " .
 			"VALUES (?)" ;
 		$params = array($ip_address);
