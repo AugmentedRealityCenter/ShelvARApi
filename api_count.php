7034fa55bdbe8d9c0fd52e9aa6426be77050078e
John Martin <johnmartin1992@yahoo.com>
2014-03-09 15:10:01 -0400

    t: p i

--- api_count.php
+++ api_count.php
@@ -371,7 +371,7 @@ function grabLastResetNotFree() {
 	$db->type = $type;
 	
 	$results = $db->fetch();
-	$test = implode($results[0]);
+	$test = implode($results[1]);
 	error_log("Results => " . $test);
 	return strtotime( $results[0] );
 }
