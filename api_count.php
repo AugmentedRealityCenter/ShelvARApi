d7508a3e46942e4dca55c8136aabce4653ad41d3
John Martin <johnmartin1992@yahoo.com>
2014-03-09 15:11:39 -0400

    removing tests

--- api_count.php
+++ api_count.php
@@ -371,9 +371,7 @@ function grabLastResetNotFree() {
 	$db->type = $type;
 	
 	$results = $db->fetch();
-	$test = implode($results[1]);
-	error_log("Results => " . $test);
-	return strtotime( $results[0] );
+	return strtotime( implode($results[0]) );
 }
 
 /**
