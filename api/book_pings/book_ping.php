22fb5f5cc5c4456cb94f92033326c4b9cf293594
Dan Bray <braydj@miamioh.edu>
2014-03-18 17:16:49 -0400

    finished book_pings error message changes

--- api/book_pings/book_ping.php
+++ api/book_pings/book_ping.php
@@ -18,22 +18,22 @@ include_once $root."header_include.php";
 include_once $root."api/api_ref_call.php";
 
 if($oauth_user['inst_activated'] != 1){
-  exit(json_encode(array('result'=>'ERROR Your institution\'s account has not yet been activated.')));
+  exit(json_encode(array('result'=>'ERROR', 'message'=>'Your institution\'s account has not yet been activated.')));
  }
 if($oauth_user['inst_has_inv'] != 1){
-  exit(json_encode(array('result'=>'ERROR Your institution does not subscribe to ShelvAR\'s inventory service.')));
+  exit(json_encode(array('result'=>'ERROR', 'message'=>'Your institution does not subscribe to ShelvAR\'s inventory service.')));
  }
 if($oauth_user['exp_date'] < time()){
-  exit(json_encode(array('result'=>'ERROR Your institution\'s account has expired. Please inform your administrator.')));
+  exit(json_encode(array('result'=>'ERROR', 'message'=>'Your institution\'s account has expired. Please inform your administrator.')));
  }
 if($oauth_user['email_verified'] != 1){
-  exit(json_encode(array('result'=>'ERROR You have not yet verified your email address.')));
+  exit(json_encode(array('result'=>'ERROR', 'message'=>'You have not yet verified your email address.')));
  }
 if($oauth_user['can_submit_inv'] != 1){
-  exit(json_encode(array('result'=>'ERROR No permission to submit data.')));
+  exit(json_encode(array('result'=>'ERROR', 'message'=>'No permission to submit data.')));
  }
 if(stripos($oauth_user['scope'],"invsubmit") === false) {
-	exit(json_encode(array('result'=>'ERROR No permission to submit data.')));
+	exit(json_encode(array('result'=>'ERROR', 'message'=>'No permission to submit data.')));
 }
 
 
@@ -43,8 +43,8 @@ $input="[]";
 if(isset($_POST["book_pings"])){
   $input = stripslashes($_POST["book_pings"]);
  }
-$ret = do_book_ping($input,$inst_id,$user_id);
-Print json_encode(array('result'=>$ret)); 
+$ret = do_book_ping($input,$inst_id,$user_id); //returns data in proper format
+Print json_encode($ret); 
 //var_dump($_POST);
 //var_dump($_GET);
 /** @endcond */  
