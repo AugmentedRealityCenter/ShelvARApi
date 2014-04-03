cf7175ae27e875763d21f80ff27b5b088489c852
Nanda Kishore, Kesanam <kesanan@miamioh.edu>
2014-04-03 17:10:55 -0400

    updated the email message

--- oauth/reset_password.php
+++ oauth/reset_password.php
@@ -24,6 +24,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['user_id']))
 		
 		$result = $db->fetch();
 		error_log(print_r($result,1));
+		$name = $result[0]['name'];
 		
 		//If there is a username that matches
 		if(count($result) > 0)
@@ -69,7 +70,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['user_id']))
 				//$to = "kesanan@miamioh.edu";
 				$to = $result[0]['email'];
 				$subject = "Your temporary password";
-				$message = "<img src='".$api."ShelvARLogo_Big.png' /><br/><br/>Dear $result[0]['name'], <br/>"."<br/>Your password to log into ShelvAR has been temporarily changed to and here is your login information:"
+				$message = "<img src='".$api."ShelvARLogo_Big.png' /><br/><br/>Dear $name, <br/>"."<br/>Your password to log into ShelvAR has been temporarily changed to and here is your login information:"."<br/>"
 																									."Username - ".$user_id."<br/>"
 																									."Password - ".$p."<br/>".
 																									"<br/>Please log in using this password and your username. At that time you may change your password to something more familiar.". "<br/>".
