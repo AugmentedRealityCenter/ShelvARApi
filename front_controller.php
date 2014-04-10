96213d8ba01bf80774c596883428492eb4ac8f35
John Martin <johnmartin1992@yahoo.com>
2014-04-10 16:38:58 -0500

    more errors msg

--- front_controller.php
+++ front_controller.php
@@ -402,7 +402,7 @@ function handle_oauth($path_arr) {
 
     include $_SERVER['DOCUMENT_ROOT'].'/path_vars_api.php';
 	include $_SERVER['DOCUMENT_ROOT'].'/api/api_ref_call.php';
-	
+	error_log("Made it past includes"); 
 
     if (count($path_arr) === 2) { // valid request
         if ($method === "GET") { // GET oauth/something_here
