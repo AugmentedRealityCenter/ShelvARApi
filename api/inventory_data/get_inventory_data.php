cedb950262c0ae2386781034117c70ac93683b93
Nanda Kishore, Kesanam <kesanan@miamioh.edu>
2014-04-15 17:48:52 -0400

    uncommented the api ref call

--- api/inventory_data/get_inventory_data.php
+++ api/inventory_data/get_inventory_data.php
@@ -1,5 +1,5 @@
 <?php
-//include_once $_SERVER['DOCUMENT_ROOT'].'/api/api_ref_call.php';
+include_once $_SERVER['DOCUMENT_ROOT'].'/api/api_ref_call.php';
 include_once $_SERVER['DOCUMENT_ROOT'].'/database.php';
 
 echo "working";
