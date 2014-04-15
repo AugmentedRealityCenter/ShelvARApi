30135ff5d1cab352d49ecc4d8ab954c76a9aac06
John Martin <johnmartin1992@yahoo.com>
2014-04-15 17:37:45 -0400

    changing path

--- front_controller.php
+++ front_controller.php
@@ -69,7 +69,7 @@ function redir($uri_path) {
 
 function api_count($inc_num) {
 	error_log("Made it to a solid reason TO blame John"); 
-    include_once $_SERVER['DOCUMENT_ROOT'].'/api_count.php'; 
+    include_once $_SERVER['DOCUMENT_ROOT'].'/api/api_count.php'; 
 
 	$method = $_SERVER['REQUEST_METHOD'];
 	
