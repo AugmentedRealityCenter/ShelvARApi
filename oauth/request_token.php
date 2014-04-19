4ccb94f2655566a899f5ee40b77ef35d218a631d
Steven Angles <anglessw@miamioh.edu>
2014-04-19 18:40:43 -0400

    removed debug log stmts from request_token

--- oauth/request_token.php
+++ oauth/request_token.php
@@ -3,20 +3,17 @@
  * @Author	Freek Lijten
  */
 
-error_log('made it to request token doc');
 require_once($_SERVER['DOCUMENT_ROOT'] . '/oauth/AutoLoader.php');
 new AutoLoader();
 
 $Provider 	= new OAuthProviderWrapper(OAuthProviderWrapper::TOKEN_REQUEST);
 $response 	= $Provider->checkOAuthRequest();
 if ($response !== true) {
-    error_log('response was not true');
 	echo $response;
 	exit;
 }
 
 try {
-    error_log('output request token');
 	$Provider->outputRequestToken();
 } catch (ProviderException $Exception) {
 	echo $Exception->getMessage();
