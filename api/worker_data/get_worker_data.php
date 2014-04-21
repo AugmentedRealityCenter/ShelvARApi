f0429d9216b09ffa6060970dc52b5a2dc04d134f
Steven Angles <anglessw@miamioh.edu>
2014-04-21 12:55:57 -0400

    beginning dev on worker_data

--- api/worker_data/get_worker_data.php
+++ api/worker_data/get_worker_data.php
@@ -2,5 +2,9 @@
 include_once $_SERVER['DOCUMENT_ROOT'].'/api/api_ref_call.php';
 include_once $_SERVER['DOCUMENT_ROOT'].'/database.php';
 
-echo "working";
+$oauth_user = get_oauth();
+$inst_id    = $oauth_user['inst_id'];
+$user_id    = $oauth_user['user_id'];
+
+echo json_encode($oauth_user);
 ?>
