14b32febb0f88bddd0f2add9d3925d8ffbfcb504
Steven Angles <anglessw@miamioh.edu>
2014-04-21 13:18:53 -0400

    fixed function call name

--- api/worker_data/get_worker_data.php
+++ api/worker_data/get_worker_data.php
@@ -2,9 +2,28 @@
 include_once $_SERVER['DOCUMENT_ROOT'].'/api/api_ref_call.php';
 include_once $_SERVER['DOCUMENT_ROOT'].'/database.php';
 
-$oauth_user = get_oauth();
+/*$oauth_user = get_oauth();
 $inst_id    = $oauth_user['inst_id'];
 $user_id    = $oauth_user['user_id'];
 
-echo json_encode($oauth_user);
+if($oauth_user['inst_activated'] != 1){
+    exit(json_encode(array('result'=>'ERROR', 'message'=>'Your institution\'s account has not yet been activated.')));
+}
+if($oauth_user['inst_has_inv'] != 1){
+    exit(json_encode(array('result'=>'ERROR', 'message'=>'Your institution does not subscribe to ShelvAR\'s inventory service.')));
+}
+if($oauth_user['exp_date'] < time()){
+    exit(json_encode(array('result'=>'ERROR', 'message'=>'Your institution\'s account has expired. Please inform your administrator.')));
+}
+if($oauth_user['email_verified'] != 1){
+    exit(json_encode(array('result'=>'ERROR', 'message'=>'You have not yet verified your email address.')));
+}
+if($oauth_user['can_read_inv'] != 1){
+    exit(json_encode(array('result'=>'ERROR', 'message'=>'No permission to read data.')));
+}
+if(stripos($oauth_user['scope'],"invread") === false) {
+    exit(json_encode(array('result'=>'ERROR', 'message'=>'No permission to read data.')));
+}*/
+
+$query = "SELECT DISTINCT user_id";
 ?>
