cd9fc78b4a145ab3a223d8a65afcaf7af4fe083f
Steven Angles <anglessw@miamioh.edu>
2014-04-22 22:40:18 -0400

    create separate handler file for individual worker data

--- front_controller.php
+++ front_controller.php
@@ -4,28 +4,28 @@ $path       = $_SERVER['REDIRECT_path'];
 $path_arr   = explode('/', $path);
 
 switch ($path_arr[0]) {
-    case "book_tags":       handle_bt($path_arr); break;
-    case "lc_numbers":      handle_lc($path_arr); break;
-    case "book_pings":      handle_bp($path_arr); break;
-    case "users":           handle_users($path_arr); break;
-    case "institutions":    handle_inst($path_arr); break;
-    case "make_tags":       handle_mt($path_arr); break;
-    case "oauth":           handle_oauth($path_arr); break;
-    case "notifications":   handle_notif($path_arr); break;
-    case 'worker_data':     handle_work($path_arr); break;
-	case 'inventory_data':	handle_inventory($path_arr); break;
-    default:                throw_error(404, "404 - not found"); break;
+case "book_tags":       handle_bt($path_arr); break;
+case "lc_numbers":      handle_lc($path_arr); break;
+case "book_pings":      handle_bp($path_arr); break;
+case "users":           handle_users($path_arr); break;
+case "institutions":    handle_inst($path_arr); break;
+case "make_tags":       handle_mt($path_arr); break;
+case "oauth":           handle_oauth($path_arr); break;
+case "notifications":   handle_notif($path_arr); break;
+case 'worker_data':     handle_work($path_arr); break;
+case 'inventory_data':	handle_inventory($path_arr); break;
+default:                throw_error(404, "404 - not found"); break;
 }
 
 /*
-    * Function to strip an extension from a string
-    * $id is the string with the extension
-    * $ext is the extension to strip from the string
-    * $ext should include the "."
-    * returns the string without the extension
-    * (and everything following it), if it is
-    * found, otherwise the original string
-    */
+ * Function to strip an extension from a string
+ * $id is the string with the extension
+ * $ext is the extension to strip from the string
+ * $ext should include the "."
+ * returns the string without the extension
+ * (and everything following it), if it is
+ * found, otherwise the original string
+ */
 function strip_ext($id, $ext) {
     // if no extension, return string
     if (!strrpos($id, $ext)) return $id;
@@ -68,18 +68,18 @@ function redir($uri_path) {
 function api_count($inc_num) {
     include_once $_SERVER['DOCUMENT_ROOT'].'/api/api_count.php'; 
 
-	$method = $_SERVER['REQUEST_METHOD'];
-	
-	error_log($_SERVER['REDIRECT_path']);
-	
-	if (is_incrementable($_SERVER['REDIRECT_path'], $method) ) {
-		increment_count($_SERVER['REDIRECT_path'], $method, $inc_num);
-		return true;
-	}
-	else {
-		throw_error(429, "429 - too many requests");
-		return false;
-	}
+    $method = $_SERVER['REQUEST_METHOD'];
+
+    error_log($_SERVER['REDIRECT_path']);
+
+    if (is_incrementable($_SERVER['REDIRECT_path'], $method) ) {
+        increment_count($_SERVER['REDIRECT_path'], $method, $inc_num);
+        return true;
+    }
+    else {
+        throw_error(429, "429 - too many requests");
+        return false;
+    }
 }
 
 /*
@@ -98,9 +98,9 @@ function handle_bt($path_arr) {
         // GET book_tags/{id}
         if ($method === "GET") { 
             $_GET['B64'] = strip_ext($path_arr[1], ".json");
-			if (api_count(1)) {
-				include $root.$get_book_tags;
-			}
+            if (api_count(1)) {
+                include $root.$get_book_tags;
+            }
         } else {
             throw_error(405, "405 - method not allowed");
         }
@@ -125,9 +125,9 @@ function handle_lc($path_arr) {
         // GET lc_numbers/{call_number}
         if ($method === "GET") { 
             $_GET['call_number'] = strip_ext($path_arr[1], ".json");
-			if (api_count(1)) {
-				include $root.$get_lc_numbers;
-			}
+            if (api_count(1)) {
+                include $root.$get_lc_numbers;
+            }
         } else {                    // some method that's not a GET
             throw_error(405, "405 - method not allowed");
         }
@@ -152,29 +152,29 @@ function handle_bp($path_arr) {
         if ($method === "GET") {  
             // GET book_pings/count
             if ($path_arr[1] === "count") { 
-				if (api_count(1)) {
-					include $root.$get_bp_count;
-				}
-            // GET book_pings/{id}
+                if (api_count(1)) {
+                    include $root.$get_bp_count;
+                }
+                // GET book_pings/{id}
             } else if ($path_arr[1] !== "") { 
                 $_GET['book_ping_id'] = strip_ext($path_arr[1], ".json");
-				if (api_count(1)) {
-					include $root.$get_bp_id;
-				}
-            // GET book_pings/
+                if (api_count(1)) {
+                    include $root.$get_bp_id;
+                }
+                // GET book_pings/
             } else if ($path_arr[1] === "") {
-				if (api_count(1)) {
-					include $root.$get_bp;
-				}
+                if (api_count(1)) {
+                    include $root.$get_bp;
+                }
             } else {                            // some other path, so throw error
                 throw_error(404, "404 - not found");
             }
         } else if ($method === "POST") {
             // POST book_pings/
             if ($path_arr[1] === "") { 
-				if (api_count(1)) {
-					include $root.$post_bp;
-				}
+                if (api_count(1)) {
+                    include $root.$post_bp;
+                }
             } else {
                 throw_error(404, "404 - not found");
             }
@@ -201,13 +201,13 @@ function handle_users($path_arr) {
 
     if ($cnt === 1) { // URI paths with a count of 1,2,3 are valid
         if ($method === 'GET') { // GET users
-			if (api_count(1)) {
-				include $root.$get_user_mult;
-			}
+            if (api_count(1)) {
+                include $root.$get_user_mult;
+            }
         } else if ($method === 'POST') { // POST users
-			if (api_count(1)) {
-				include $root.$post_users;
-			}
+            if (api_count(1)) {
+                include $root.$post_users;
+            }
         } else {
             throw_error(405, '405 - method not allowed');
         }
@@ -216,23 +216,23 @@ function handle_users($path_arr) {
         if ($method === 'GET') { 
             // GET users/activate_email
             if ($path_arr[1] === 'activate_email') { 
-				if (api_count(1)) {
-					include $root.$get_act_email;
-				}
-            // GET users/some_user.json
+                if (api_count(1)) {
+                    include $root.$get_act_email;
+                }
+                // GET users/some_user.json
             } else {
                 $_GET['user_id'] = strip_ext($path_arr[1], '.json');
-				if (api_count(1)) {
-					include $root.$get_user;
-				}
+                if (api_count(1)) {
+                    include $root.$get_user;
+                }
             }
-        // POST users/something_here
+            // POST users/something_here
         } else if ($method === 'POST') { 
             // POST users/edit
             if ($path_arr[1] === 'edit') { 
-				if (api_count(1)) {
-					include $root.$post_users_edit;
-				}
+                if (api_count(1)) {
+                    include $root.$post_users_edit;
+                }
             } else {
                 throw_error(404, '404 - not found');
             }
@@ -245,21 +245,21 @@ function handle_users($path_arr) {
             // GET users/{id}/permissions
             if ($path_arr[2] === 'permissions') { 
                 $_GET['user_id'] = $path_arr[1];
-				if (api_count(1)) {
-					include $root.$get_user_perm;
-				}
-            // GET users/available/{id}.json
+                if (api_count(1)) {
+                    include $root.$get_user_perm;
+                }
+                // GET users/available/{id}.json
             } else if ($path_arr[1] === 'available') {
                 $_GET['user_id'] = strip_ext($path_arr[2], '.json');
-				if (api_count(1)) {
-					include $root.$get_users_avail;
-				}
-            // GET users/email_registered/{id}
+                if (api_count(1)) {
+                    include $root.$get_users_avail;
+                }
+                // GET users/email_registered/{id}
             } else if ($path_arr[1] === 'email_registered') {
                 $_GET['email'] = strip_ext($path_arr[2], '.json');
-				if (api_count(1)) {
-					include $root.$get_email_reg;
-				}
+                if (api_count(1)) {
+                    include $root.$get_email_reg;
+                }
             } else {
                 throw_error(404, '404 - not found');
             }
@@ -267,9 +267,9 @@ function handle_users($path_arr) {
             // POST users/{id}/permissions
             if ($path_arr[2] === 'permissions') { 
                 $_POST['user_id'] = $path_arr[1];
-				if (api_count(1)) {
-					include $root.$post_users_perm;
-				}
+                if (api_count(1)) {
+                    include $root.$post_users_perm;
+                }
             } else {
                 throw_error(404, '404 - not found');
             }
@@ -299,32 +299,32 @@ function handle_inst($path_arr) {
         if ($method === 'GET') {
             // GET institutions/
             if ($path_arr[1] === '') {
-				if (api_count(1)) {
-					include $root.$get_inst_mult;
-				}
-            // GET institutions/activate_inst
+                if (api_count(1)) {
+                    include $root.$get_inst_mult;
+                }
+                // GET institutions/activate_inst
             } else if ($path_arr[1] === 'activate_inst') {
-				if (api_count(1)) {
-					include $root.$get_act_inst;
-				}
-            // GET institutions/{id}
+                if (api_count(1)) {
+                    include $root.$get_act_inst;
+                }
+                // GET institutions/{id}
             } else {
                 $_GET['inst_id'] = strip_ext($path_arr[1], '.json');
-				if (api_count(1)) {
-					include $root.$get_inst;
-				}
+                if (api_count(1)) {
+                    include $root.$get_inst;
+                }
             }
         } else if ($method === 'POST') {
             // POST institutions/
             if ($path_arr[1] === '') {
-				if (api_count(1)) {
-					include $root.$post_inst_reg;
-				}
-            // POST institutions/edit
+                if (api_count(1)) {
+                    include $root.$post_inst_reg;
+                }
+                // POST institutions/edit
             } else if ($path_arr[1] === 'edit') {
-				if (api_count(1)) {
-					include $root.$post_inst_edit;
-				}
+                if (api_count(1)) {
+                    include $root.$post_inst_edit;
+                }
             } else {
                 throw_error(404, '404 - not found');
             }
@@ -335,9 +335,9 @@ function handle_inst($path_arr) {
             // GET institutions/available/{id}
             if ($path_arr[1] === 'available') {
                 $_GET['inst_id'] = strip_ext($path_arr[2], '.json');
-				if (api_count(1)) {
-					include $root.$get_inst_avail;
-				}
+                if (api_count(1)) {
+                    include $root.$get_inst_avail;
+                }
             } else {
                 throw_error(404, '404 - not found');
             }
@@ -366,15 +366,15 @@ function handle_mt($path_arr) {
         if ($method === "GET") { 
             // GET make_tags/paper_formats
             if ($path_arr[1] === "paper_formats") {
-				if (api_count(1)) {
-					include $root.$get_formats;
-				}
-            // GET make_tags/something_else
+                if (api_count(1)) {
+                    include $root.$get_formats;
+                }
+                // GET make_tags/something_else
             } else {
                 $_GET['type'] = strip_ext($path_arr[1], ".pdf");
-				if (api_count(1)) {
-					include $root.$get_tags;
-				}
+                if (api_count(1)) {
+                    include $root.$get_tags;
+                }
             }
         } else {
             throw_error(405, "405 - method not allowed");
@@ -385,10 +385,10 @@ function handle_mt($path_arr) {
 }
 
 /*
-* ------
-* oauth/
-* ------
-*/
+ * ------
+ * oauth/
+ * ------
+ */
 function handle_oauth($path_arr) {
     $root = $_SERVER['DOCUMENT_ROOT']."/";
     $method = $_SERVER['REQUEST_METHOD'];
@@ -400,31 +400,31 @@ function handle_oauth($path_arr) {
             // determine and dispatch necessary file
             switch($path_arr[1]) {
             case "get_request_token":  
-					include $root.$get_req_token;	
-				break;
+                include $root.$get_req_token;	
+                break;
             case "login":    
-					include $root.$get_login; 	
-				break;
+                include $root.$get_login; 	
+                break;
             case "get_access_token":  
-					include $root.$get_acc_token; 
-				break;
+                include $root.$get_acc_token; 
+                break;
             case "whoami":   
-					include $root.$get_whoami;
-				break;
+                include $root.$get_whoami;
+                break;
             case "post_login": 
-					include $root.$get_post_login; 
-				break;
+                include $root.$get_post_login; 
+                break;
             default: throw_error(404, "404 - not found"); break;
             }
         } else if ($method === "POST") {
             if ($path_arr[1] === "login") {
-					include $root.$get_login;
+                include $root.$get_login;
             } else if ($path_arr[1] === 'post_login') {
-					include $root.$get_post_login;
+                include $root.$get_post_login;
             } else if ($path_arr[1] === 'get_request_token') {
-					include $root.$get_req_token;
+                include $root.$get_req_token;
             } else if ($path_arr[1] === 'get_access_token') {
-					include $root.$get_acc_token;
+                include $root.$get_acc_token;
             }
         } else {
             throw_error(405, "405 - method not allowed");
@@ -450,7 +450,7 @@ function handle_notif($path_arr) {
         // GET notifications/{inst_id}
         if ($method === "GET") { 
             $_GET['inst_id'] = $path_arr[1];
-				include $root.$get_notif;
+            include $root.$get_notif;
         } else {                    // some method that's not a GET
             throw_error(405, "405 - method not allowed");
         }
@@ -478,6 +478,13 @@ function handle_work($path_arr) {
         } else {
             throw_error(405, '405 - method not allowed');
         }
+    } else if ($cnt === 2 && $path_arr[1] !== '') {
+        // GET worker_data/{id}
+        if ($method === 'GET') {
+            include $root.$get_worker_id;
+        } else {
+            throw_error(405, '405 - method not allowed');
+        } 
     } else {
         throw_error(404, '404 - not found');
     } 
--- path_vars_api.php
+++ path_vars_api.php
@@ -1,36 +1,37 @@
 <?php
-    // get path constants
-    $get_book_tags      = "api/lc2bin/book_tags.php";
-    $get_lc_numbers     = "api/lc2bin/lc_numbers.php";
-    $get_bp_count       = "api/book_pings/get_ping_count_book.php";
-    $get_bp_id          = "api/book_pings/get_by_id.php";
-    $get_user_perm      = "api/users/get_permissions.php";
-    $get_act_email      = "api/users/activate_email.php";
-    $get_users_avail    = "api/users/user_available.php";
-    $get_email_reg      = "api/users/email_registered.php";
-    $get_user           = "api/users/get_user.php";
-    $get_user_mult      = "api/users/get_users.php";
-    $get_act_inst       = "api/institutions/activate_inst.php";
-    $get_inst_avail     = "api/institutions/inst_available.php";
-    $get_inst           = "api/institutions/get_institution.php";
-    $get_inst_mult      = "api/institutions/get_institutions.php";
-    $get_bp             = "api/book_pings/book_pings_get.php";
-    $get_tags           = "api/tagmaker/pdfPrint2.php";
-    $get_formats        = "api/tagmaker/tagformats.json";
-    $get_whoami         = "api/oauth/whoami.php";
-    $get_acc_token      = "oauth/access_token.php";
-    $get_req_token      = "oauth/request_token.php";
-    $get_login          = "oauth/login.php";
-    $get_notif          = "api/notifications/get_inst_notification.php";
-    $get_post_login     = "oauth/post-login.php";
-    $get_worker_data    = 'api/worker_data/get_worker_data.php';
-	$get_inventory_data = 'api/inventory_data/get_inventory_data.php';
+// get path constants
+$get_book_tags      = "api/lc2bin/book_tags.php";
+$get_lc_numbers     = "api/lc2bin/lc_numbers.php";
+$get_bp_count       = "api/book_pings/get_ping_count_book.php";
+$get_bp_id          = "api/book_pings/get_by_id.php";
+$get_user_perm      = "api/users/get_permissions.php";
+$get_act_email      = "api/users/activate_email.php";
+$get_users_avail    = "api/users/user_available.php";
+$get_email_reg      = "api/users/email_registered.php";
+$get_user           = "api/users/get_user.php";
+$get_user_mult      = "api/users/get_users.php";
+$get_act_inst       = "api/institutions/activate_inst.php";
+$get_inst_avail     = "api/institutions/inst_available.php";
+$get_inst           = "api/institutions/get_institution.php";
+$get_inst_mult      = "api/institutions/get_institutions.php";
+$get_bp             = "api/book_pings/book_pings_get.php";
+$get_tags           = "api/tagmaker/pdfPrint2.php";
+$get_formats        = "api/tagmaker/tagformats.json";
+$get_whoami         = "api/oauth/whoami.php";
+$get_acc_token      = "oauth/access_token.php";
+$get_req_token      = "oauth/request_token.php";
+$get_login          = "oauth/login.php";
+$get_notif          = "api/notifications/get_inst_notification.php";
+$get_post_login     = "oauth/post-login.php";
+$get_worker_data    = 'api/worker_data/get_worker_data.php';
+$get_worker_id      = "api/worker_data/get_by_id.php";
+$get_inventory_data = 'api/inventory_data/get_inventory_data.php';
 
-    // post path constants
-    $post_bp            = "api/book_pings/book_ping.php";
-    $post_users_edit    = "api/users/edit_user.php";
-    $post_users_perm    = "api/users/edit_permissions.php";
-    $post_users         = "api/users/register_user.php";
-    $post_inst_edit     = "api/institutions/edit_institution.php";
-    $post_inst_reg      = "api/institutions/register_institution.php";
+// post path constants
+$post_bp            = "api/book_pings/book_ping.php";
+$post_users_edit    = "api/users/edit_user.php";
+$post_users_perm    = "api/users/edit_permissions.php";
+$post_users         = "api/users/register_user.php";
+$post_inst_edit     = "api/institutions/edit_institution.php";
+$post_inst_reg      = "api/institutions/register_institution.php";
 ?>
