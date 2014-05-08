86c0cc9258505ac3a9a7905e0cf3fe9219f911e1
John Martin <johnmartin1992@yahoo.com>
2014-05-08 12:38:10 -0400

    removing some messages

--- api/api_count.php
+++ api/api_count.php
@@ -470,12 +470,8 @@ function handleIPAddress() {
 	
 	$results = $db->fetch();
 	
-	//I believe it is adding a new IP every time
-	
-	
 	
 	if ($results == NULL || count($results) == 0) {
-		error_log("New IP address added to database");
 		$query = "INSERT INTO unknown_users(ip_address) " .
 			"VALUES (?)" ;
 		$params = array($ip_address);
